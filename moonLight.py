ffe2e4e39c9e5366e58e2cc73039314dfb4bd185
Nixon Ball <nixon.ball@farragut.com>
2016-09-28 19:10:56 -0400

    updated to grab phase name as well as percent

--- moonLight.py
+++ moonLight.py
@@ -18,3 +18,20 @@ phasePercent = phasePercentSpan.get_text()
 
 print phasePercent
 
+qlook = soup.find('div', {'id': 'qlook'})
+
+phaseName = soup.find('p').get_text()
+
+print phaseName
+
+lightNumber = 0
+
+if phaseName = 'Waning Crescent':
+	lightNumber = 1
+elif phaseName = 'Third Quarter':
+	lightNumber = 3
+elif phaseName = 'Waning Gibbous':
+	lightNumber = 5
+elif phaseName = 'Full':
+	lightNumber = 7
+
