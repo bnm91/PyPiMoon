efece3bedf4993d7ed6b74b89b2359d5ec2cbf5f
Nixon Ball <nixon.ball@farragut.com>
2016-09-29 12:42:28 -0400

    cleaned a little and added light comments

--- moonLight.py
+++ moonLight.py
@@ -1,30 +1,34 @@
 from bs4 import BeautifulSoup
-
 import lxml
-
 import requests
-
 import time
 
 
-r = requests.get('http://www.timeanddate.com/moon/phases/usa/raleigh')
+#get BeautifulSoup for raleigh moon phase page
+r = requests.get('http://www.timeanddate.com/moon/phases/usa/raleigh') #permalink
 
 data = r.text
 
 soup = BeautifulSoup(data, 'lxml')
 
+
+#find moon percent
 phasePercentSpan = soup.find('span', {'id': 'cur-moon-percent'})
 
 phasePercent = phasePercentSpan.get_text()
 
 print phasePercent
 
+
+#find the phase name
 qlook = soup.find('div', {'id': 'qlook'})
 
 phaseName = soup.find('p').get_text()
 
 print phaseName
 
+
+#determine how lit up the room moon should be
 lightNumber = 0
 
 if phaseName = 'Waning Crescent':
